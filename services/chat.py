1564615dfaf255a3729b81a665308422483cd5a2
Ria Chandra <56750743+riachandra02@users.noreply.github.com>
2024-11-01 13:23:17 +0530

    Update chat.py

--- services/chat.py
+++ services/chat.py
@@ -1,1 +1,10 @@
+# services/chat.py
+from services.chatutils import (
+    get_pdf_text,
+    get_text_chunks,
+    get_vector_store,
+    get_conversational_chain,
+    search_internet,
+    normalize_question,
+)
 
