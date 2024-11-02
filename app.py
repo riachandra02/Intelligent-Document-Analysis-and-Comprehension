f721d642c0f18b0cf6ec8d71d64a927c8f0b23d3
Ria Chandra <56750743+riachandra02@users.noreply.github.com>
2024-11-02 15:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 from flask import Flask
 from routes.chat import chat_bp  # Import your routes
 from routes.summ import summ_bp
+from routes.external import fetch_external_bp
 
 app = Flask(__name__)
 
 # Register the blueprint
 app.register_blueprint(chat_bp)
 app.register_blueprint(summ_bp)
+app.register_blueprint(fetch_external_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
