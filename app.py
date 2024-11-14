5e8947f28137d705084c115dde33089f6894cec5
Ria Chandra <56750743+riachandra02@users.noreply.github.com>
2024-11-14 17:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # app.py
 from flask import Flask
-from routes.chat import chat_bp  # Import your routes
+from routes.chat import chat_bp
 from routes.summ import summ_bp
-from routes.external import fetch_external_bp
-from routes.voice import voice_bp
+from routes.external import external_bp
+from routes.voice_interactive import voice_interactive_bp
 
 app = Flask(__name__)
 
 # Register the blueprint
 app.register_blueprint(chat_bp)
 app.register_blueprint(summ_bp)
-app.register_blueprint(fetch_external_bp)
-app.register_blueprint(voice_bp)
+app.register_blueprint(external_bp, url_prefix='/api')
+app.register_blueprint(voice_interactive_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
